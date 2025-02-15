ec544078c0a58aa57b6476c53a01a61bfc9d03d2
Victoria Vizmonte <victoriaangela.vizmonte@gmail.com>
2025-02-15 11:48:53 +0000

    First terraform storage bucket exercise

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform{
+    required_providers {
+      google = {
+        source = "hashicorp/google"
+        version = "~> 5.0"
+      }
+    }
+}
+
+provider "google" {
+  project = "fleet-aleph-451009-q7" # Replace with Actual Project ID
+  region = "us-west1"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name = "my-terraform-bucket-40598112523" # Ensure globally unique
+  location = "us-west1"
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.my_bucket.name
+}
